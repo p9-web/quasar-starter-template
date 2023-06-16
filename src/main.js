838273b7f4c1411c17e5a0937b3677d4d1db0399
Pierre Côté <58827453+p9-web@users.noreply.github.com>
2023-06-16 12:05:04 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/material-symbols-outlined/material-symbols-outlined.css'
+import '@quasar/extras/material-symbols-rounded/material-symbols-rounded.css'
+import '@quasar/extras/material-symbols-sharp/material-symbols-sharp.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+// Assumes your root component is App.vue
+// and placed in same folder as main.js
+import App from './App.vue'
+
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+})
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app')
